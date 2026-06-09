384d44208991ddbbe44d4d8087cf68ea1b341be2
Paulio <pauliorandall@gmail.com>
2026-06-09 20:53:14 +0100

    Add some docs

--- example_app/example_app.go
+++ example_app/example_app.go
@@ -1,23 +1,61 @@
 package example_app
 
 import (
+	// The gogpu "eco system?" has a highly decoupled and
+	// layered architecture. We must pick imports based on
+	// our needs to build the graphics stack for an
+	// application. There is no God import.
+	// github.com/gogpu
+
+	// gg/gpu enables hardware-accelerated rendering, if
+	// supports it. Falls back to using CPU if not.
+	_ "github.com/gogpu/gg/gpu"
+
+	// gogpu/gogpu high-level API for graphics including
+	// window management. The UI toolkit this example app is
+	// built on uses this API.
 	_g "github.com/gogpu/gogpu"
+
+	// gogpu/ui/app provides framework structures for us to
+	// configure and populate with our widgets and buisness
+	// logic.
 	_a "github.com/gogpu/ui/app"
+
+	// gogpu/ui/desktop provides an environment to run the
+	// gogpu/ui/app App we configured and populated with our
+	// widgets and business logic.
 	_d "github.com/gogpu/ui/desktop"
 
+	// clickable_area is a custom widget I created that
+	// detects mouse clicks and places boxes containing the
+	// mouse click location at the mosue click location.
 	"github.com/PaulioRandall/exploring-gogpu/clickable_area"
 )
 
 type ExampleApp struct {
+	// gogpuApp is stored here because we need it to run
+	// uiApp.
 	gogpuApp *_g.App
-	uiApp    *_a.App
+
+	// uiApp contains our custom widget, it's stored here so
+	// we can run it when the Run method is called.
+	uiApp *_a.App
 }
 
+// NewExampleApp builds and returns our app, it does not
+// run it.
 func NewExampleApp() *ExampleApp {
+	// Create a gogpu.App that will power our ui/app.App.
 	gogpuApp := createGogpuApp()
+
+	// Create a ui/app.App powered by the gogpu.App we
+	// created above.
 	uiApp := createUiApp(gogpuApp)
 
+	// Create an instance of the custom widget I created.
 	clickableArea := clickable_area.NewClickableArea()
+
+	// Set the custom widget as the top-level (root) widget.
 	uiApp.SetRoot(clickableArea.AsWidget())
 
 	return &ExampleApp{
@@ -43,7 +81,8 @@ func createUiApp(gogpuApp *_g.App) *_a.App {
 	)
 }
 
+// Run runs our built ExampleApp. It blocks so this
+// function won't exit until our app has closed.
 func (ea *ExampleApp) Run() error {
-	// Blocks thread.
 	return _d.Run(ea.gogpuApp, ea.uiApp)
 }
